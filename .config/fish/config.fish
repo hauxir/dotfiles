3a104035c2f4c64e70df15f28aba4006af6ecc9c
Haukur Rosinkranz <hauxir@gmail.com>
2021-07-28 16:55:05 +0200

    fish tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,9 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Theme
 set fish_theme robbyrussell
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-set fish_plugins django gi ndenv emoji-clock node pyenv extract percol python tmux
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
 
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-eval sh $HOME/.config/base16-shell/base16-tomorrow.dark.sh
 alias vim=nvim
