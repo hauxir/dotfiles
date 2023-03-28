33f97149c6a9c0484df93a813ba6fbca56fd4497
Haukur Rósinkranz <hauxir@gmail.com>
2023-03-28 22:34:13 +0000

    Delete gitsigns.lua

--- .config/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-require('gitsigns').setup({
-  keymaps = {
-    noremap = true,
-    buffer = true,
-    ['n gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n gb'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
-  },
-})
