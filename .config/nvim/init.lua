2f4d9882445591841d4956d591fcaef98299d412
Haukur Rósinkranz <hauxir@gmail.com>
2023-03-28 23:12:43 +0000

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,6 @@ require('options')
 require('pack')
 
 require('plugins.compe')
-require('plugins.gitsigns')
 require('plugins.lspconfig')
 require('plugins.telescope')
 require('plugins.treesitter')
