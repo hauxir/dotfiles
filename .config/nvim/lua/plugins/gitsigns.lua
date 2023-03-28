65a7603e6c145a3f38fe9dd2e70be0967eef1967
Haukur Rósinkranz <hauxir@gmail.com>
2023-03-28 22:31:52 +0000

    Update gitsigns.lua

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,7 +5,4 @@ require('gitsigns').setup({
     ['n gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
     ['n gb'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
   },
-  current_line_blame_delay=0,
-  use_decoration_api = true,
-  use_internal_diff = true,
 })
