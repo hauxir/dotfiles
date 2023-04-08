a95cfd49fe9399ef4c9edae99f7b28a3d6588cfe
Haukur Rósinkranz <hauxir@gmail.com>
2023-04-08 14:43:55 +0000

    Update chatgpt.lua

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -1,3 +1,4 @@
 require("chatgpt").setup({})
 local set_keymap = require('../utils').set_keymap
 set_keymap('n', '<leader>a', '<cmd> ChatGPTEditWithInstructions<cr>')
+set_keymap('v', '<leader>a', '<cmd> ChatGPTEditWithInstructions<cr>')
