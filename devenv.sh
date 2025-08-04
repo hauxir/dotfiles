90ac34d052c40a731352b8308c845cd00ad27210
Haukur Rósinkranz <hauxir@gmail.com>
2025-08-04 13:42:06 +0000

    Update devenv.sh

--- devenv.sh
+++ devenv.sh
@@ -29,7 +29,7 @@ RUN apt-get install -y \
     libxml2-utils \
     wget
 
-RUN curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+RUN curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
 
 RUN apt-get update
 
